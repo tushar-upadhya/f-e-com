f424cb1e72593a17b0a34a6b19e3796aafc0c9dd
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-05-07 10:49:26 +0530

    handleCancelReservation

--- client/components/CancelReservation.tsx
+++ client/components/CancelReservation.tsx
@@ -1,9 +1,50 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
 
 const CancelReservation = ({ reservation }: { reservation: any }) => {
-  return <div>CancelReservation</div>;
+  const handleCancelReservation = (id: number) => {
+    console.log("id:", id);
+  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button size={"md"}>Cancel Reservation</Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        {/* header */}
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        {/* footer */}
+        <AlertDialogFooter>
+          <AlertDialogCancel>Dismiss</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => handleCancelReservation(reservation.id)}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 };
 
 export default CancelReservation;
